3531101112489dfca9f63f9497a474da7f55b261
lananhhoang88 <67369919+lananhhoang88@users.noreply.github.com>
2022-06-02 07:58:14 +0700

    j54

--- routes/dm-chucvu.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { crud } from '../controllers/dm-chucvu';
-import { addRoute } from '../utils/route';
-
-const baseController = crud();
-const router = express.Router();
-addRoute(router, 'dm_chucvu', baseController);
-
-export default router;
--- routes/dm-loainguoidung.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { crud } from '../controllers/dm-loainguoidung';
-import { addRoute } from '../utils/route';
-
-const baseController = crud();
-const router = express.Router();
-addRoute(router, 'dm_loainguoidung', baseController);
-
-export default router;
