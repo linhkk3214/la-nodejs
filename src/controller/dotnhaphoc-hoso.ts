d085f4820c56a5cb2b2a1a576a93f4ece5f2d1d0
LINK\linhdh <linh.dino.ptit@gmail.com>
2022-06-07 08:20:38 +0700

    no message

--- src/controller/dotnhaphoc-hoso.ts
+++
@@ -1,8 +0,0 @@
-import { BaseController } from '../base/base-controller';
-import DotNhapHoc_HoSo from '../models/dotnhaphoc-hoso';
-
-export class DotNhapHoc_HoSoController extends BaseController {
-    constructor() {
-        super(DotNhapHoc_HoSo);
-    }
-}
