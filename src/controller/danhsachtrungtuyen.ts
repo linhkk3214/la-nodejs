5f4202001c79e25568357ad9a02a602453cf963d
lananhhoang88 <67369919+lananhhoang88@users.noreply.github.com>
2022-06-07 22:12:34 +0700

    dhtt

--- src/controller/danhsachtrungtuyen.ts
+++ src/controller/danhsachtrungtuyen.ts
@@ -1,8 +1,12 @@
 import { BaseController } from '../base/base-controller';
-import DanhSachTrungTuyen from '../models/danhsachtrungtuyen';
+import DanhSachTrungTuyen, { IDanhSachTrungTuyen } from '../models/danhsachtrungtuyen';
 
 export class DanhSachTrungTuyenController extends BaseController {
     constructor() {
         super(DanhSachTrungTuyen);
+    }
+
+    override beforeSave(model: IDanhSachTrungTuyen): void {
+        model.hoVaTen = `${model.ho} ${model.ten}`;
     }
 }
