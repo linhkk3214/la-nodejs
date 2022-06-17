f6472b267e2ec6d7ad0b3332f8c3bf0e13b45c8d
linhdh <linh.dino.ptit@gmail.com>
2022-06-17 21:37:17 +0700

    base

--- src/controller/hosonguoihoc.ts
+++ src/controller/hosonguoihoc.ts
@@ -1,11 +1,49 @@
 import { BaseController } from '../base/base-controller';
 import HoSoNguoiHoc, { IHoSoNguoiHoc } from '../models/hosonguoihoc';
+import DanhSachLopHanhChinh from '../models/danhsachlophanhchinh';
 
 export class HoSoNguoiHocController extends BaseController {
+    itemOld: IHoSoNguoiHoc;
     constructor() {
         super(HoSoNguoiHoc);
     }
-    override async beforeSave(model: IHoSoNguoiHoc) {
+
+    override async beforeSave(model: IHoSoNguoiHoc, isEdit: boolean = false) {
         model.hoVaTen = `${model.Ho} ${model.Ten}`;
+        if (isEdit) {
+            this.itemOld = await HoSoNguoiHoc.findOne({ _id: model._id });
+        }
+    }
+
+    override async afterInsert(model: IHoSoNguoiHoc) {
+        // Cập nhật sĩ số lớp hành chính
+        if (model.idLopHanhChinh) {
+            await this.updateSiSoLopHanhChinh(model.idLopHanhChinh.toString());
+        }
+    }
+
+    override async afterUpdate(model: IHoSoNguoiHoc) {
+        if (this.itemOld) {
+            if (this.itemOld.idLopHanhChinh != model.idLopHanhChinh) {
+                if (model.idLopHanhChinh) {
+                    await this.updateSiSoLopHanhChinh(model.idLopHanhChinh.toString());
+                }
+                if (this.itemOld.idLopHanhChinh) {
+                    await this.updateSiSoLopHanhChinh(this.itemOld.idLopHanhChinh.toString());
+                }
+            }
+        }
+        else if (model.idLopHanhChinh) {
+            await this.updateSiSoLopHanhChinh(model.idLopHanhChinh.toString());
+        }
+    }
+
+    private async updateSiSoLopHanhChinh(idLopHanhChinh: string) {
+        const count = await HoSoNguoiHoc.count({ idLopHanhChinh: idLopHanhChinh });
+        await DanhSachLopHanhChinh.updateOne({ _id: idLopHanhChinh }, {
+            $set: {
+                siSo: count
+            }
+        });
     }
 }
